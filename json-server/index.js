d97f5097c34a5541f01d5fc459335195752cf5eb
Danil Dmitriev <danil.dmitriev.2014@inbox.ru>
2024-01-22 15:07:31 +0300

    lesson #31.1 - add and configure json-server

---
+++ json-server/index.js
@@ -0,0 +1,50 @@
+const fs = require("fs");
+const jsonServer = require("json-server");
+const path = require("path");
+
+const server = jsonServer.create();
+
+const router = jsonServer.router(path.resolve(__dirname, "db.json"));
+
+server.use(jsonServer.defaults({}));
+server.use(jsonServer.bodyParser);
+
+server.use(async (req, res, next) => {
+  await new Promise((res) => {
+    setTimeout(res, 800);
+  });
+  next();
+});
+
+server.post("/login", (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const db = JSON.parse(fs.readFileSync(path.resolve(__dirname, "db.json"), "UTF-8"));
+    const { users = [] } = db;
+
+    const userFromBd = users.find((user) => user.username === username && user.password === password);
+
+    if (userFromBd) {
+      return res.json(userFromBd);
+    }
+
+    return res.status(403).json({ message: "User not found" });
+  } catch (e) {
+    console.log(e);
+    return res.status(500).json({ message: e.message });
+  }
+});
+
+server.use((req, res, next) => {
+  if (!req.headers.authorization) {
+    return res.status(403).json({ message: "Auth error" });
+  }
+
+  next();
+});
+
+server.use(router);
+
+server.listen(5000, () => {
+  console.log("server is running on 5000 port");
+});
