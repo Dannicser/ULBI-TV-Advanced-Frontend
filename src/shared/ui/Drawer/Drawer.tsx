829e336cd738152f8812601fa1bc469ced0c1548
Danil Dmitriev <danil.dmitriev.2014@inbox.ru>
2024-03-21 13:36:00 +0300

    lesson #82 - Drawer upgrade using react-spring/web and use-gesture/react libs

--- src/shared/ui/Drawer/Drawer.tsx
+++ src/shared/ui/Drawer/Drawer.tsx
@@ -1,15 +1,14 @@
 import { classNames, Mods } from "shared/lib/classNames/classNames";
-import { memo, ReactNode } from "react";
+import { memo, ReactNode, useCallback, useEffect } from "react";
 
 import { Overlay } from "../Overlay/Overlay";
 
 import { Portal } from "../Portal/Portal";
 
-import { useModal } from "shared/lib/hooks/useModal";
-
 import cls from "./Drawer.module.scss";
 
-const ANIMATION_DELAY = 300;
+import { useDrag } from "@use-gesture/react";
+import { a, useSpring, config } from "@react-spring/web";
 
 interface DrawerProps {
   className?: string;
@@ -19,25 +18,72 @@ interface DrawerProps {
   isLazy?: boolean;
 }
 
+const height = window.innerHeight - 100;
+
 export const Drawer = memo((props: DrawerProps) => {
   const { className, children, onClose, isOpen, isLazy } = props;
 
-  const { close, isClosing, isMounted } = useModal({ animationDelay: ANIMATION_DELAY, onClose, isOpen });
+  const [{ y }, api] = useSpring(() => ({ y: height }));
 
-  const mods: Mods = {
-    [cls.opened]: isOpen,
-    [cls.isClosing]: isClosing,
-  };
-
-  if (isLazy && !isMounted) {
+  if (isLazy) {
     return null;
   }
 
+  const openDrawer = () => {
+    api.start({ y: 0, immediate: false });
+  };
+
+  useEffect(() => {
+    if (isOpen) {
+      openDrawer();
+    }
+  }, [isOpen, openDrawer, api]);
+
+  const close = (velocity = 0) => {
+    api.start({
+      y: height,
+      immediate: false,
+      config: { ...config.stiff, velocity },
+
+      onResolve: onClose,
+    });
+  };
+
+  const bind = useDrag(
+    ({ last, velocity: [, vy], direction: [, dy], movement: [, my], cancel }) => {
+      if (my < -70) cancel();
+
+      if (last) {
+        if (my > height * 0.5 || (vy > 0.5 && dy > 0)) {
+          close();
+        } else {
+          openDrawer();
+        }
+      } else {
+        api.start({ y: my, immediate: true });
+      }
+    },
+    {
+      from: () => [0, y.get()],
+      filterTaps: true,
+      bounds: { top: 0 },
+      rubberband: true,
+    }
+  );
+
+  if (!isOpen) {
+    return null;
+  }
+
+  const display = y.to((py) => (py < height ? "block" : "none"));
+
   return (
     <Portal element={document.body}>
-      <div className={classNames(cls.Drawer, mods, [className])}>
+      <div className={classNames(cls.Drawer, {}, [className, "", ""])}>
         <Overlay onClick={close} />
-        <div className={cls.content}>{children}</div>
+        <a.div className={cls.sheet} style={{ display, bottom: `calc(-100vh + ${height - 100}px)`, y }} {...bind()}>
+          {children}
+        </a.div>
       </div>
     </Portal>
   );
